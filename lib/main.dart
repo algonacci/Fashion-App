d9cdf802c2c9c3197d0683dd2c2a4e766433a3aa
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-07-02 12:50:26 +0700

    Init UI Design

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -25,6 +26,70 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera_alt,
+              color: Colors.deepPurple.shade500,
+            ),
+          ),
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          'Fashion Design',
+          style: TextStyle(
+            color: Colors.deepPurple,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            color: Colors.pink.shade300,
+            height: 150,
+            width: double.infinity,
+            child: ListView(
+              padding: EdgeInsets.all(12),
+              scrollDirection: Axis.horizontal,
+              children: [
+                listElement(
+                  'assets/images/model-1.jpg',
+                  'assets/images/chanellogo.jpg',
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  listElement(String imagePath, String logoPath) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.all(12),
+              height: 75,
+              width: 75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(46),
+                image: DecorationImage(
+                  image: AssetImage(
+                    imagePath,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
